8e4c0624e306f261011dd38db94a7ea931520629
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-01-19 16:59:56 +0100

    Function To Return Random Element For The Game Is Created

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+//function to get the choice of the computer
+function getComputerChoice(){
+    // creating a container for the rock,paper,scissors elements
+    let elements = ['rock','paper','scissors']
+
+    //container for returning random element
+    let randomElement = elements[Math.floor(Math.random() * 3)]
+    return (console.log(randomElement))
+
+}
+
+getComputerChoice()
